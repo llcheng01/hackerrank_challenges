ddabba124b2db303c27ea2225291b1584b152060
Johnny Cheng <johnny.cheng7b9@gmail.com>
2014-05-31 19:11:04 -0700

    Complete simple test cases. Streamline code

--- lib/tree.rb
+++ lib/tree.rb
@@ -1,24 +1,8 @@
-# 
+# tree.rb
 class Tree
-    attr_reader :cycle
-
-    def initialize(cycle=0)
-       if cycle < 0 && cycle > 60
-           raise "Cycle must be within range"
-       end
-
-        @cycle=cycle
-    end
-
-    def grow
-        if (cycle == 0)
-            return 1
-        else
-            if (cycle % 2 == 0)
-                grow(cycle - 1) + 1
-            else
-                grow(cycle-1) * 2
-            end
-        end
+    def grow(cycle)
+        raise RangeError, "Cycle must be within range" if (cycle < 0 || cycle > 60)
+        return 1 if (cycle == 0)
+        (cycle % 2 == 0) ? grow(cycle - 1) + 1 : grow(cycle-1) * 2
     end
 end
--- spec/tree_spec.rb
+++ spec/tree_spec.rb
@@ -1,25 +1,17 @@
 require_relative '../lib/tree'
 
 describe Tree, '#growth' do
-    it "should handle initialize" do
-        tree = Tree.new(10)
-
-        expect(tree.cycle).to eql(10)
-
-    end
-
-    it "calculate default height" do
-        tree = Tree.new(0)
-        
-        expect(tree.grow()).to eql(1)
-    end
-    
     it "can handle 0 <= N <= 60 cycle limits" do
-        # tree = Tree.new(65)
-
-    end 
+        tree = Tree.new()
+        expect{tree.grow(65)}.to raise_error(RangeError, /within range/)
+    end
+    it "calculate default height" do
+        tree = Tree.new()
+        expect(tree.grow(0)).to eql(1)
+    end
 
     it "can calculate correct cycle" do
-        tree = Tree.new(5)
+        tree = Tree.new()
+        expect(tree.grow(5)).to eql(14)
     end
 end
