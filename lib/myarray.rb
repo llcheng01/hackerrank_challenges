6ea4429611d983ed40ef9c52dea75ba144ffac17
Johnny Cheng <johnny.cheng7b9@gmail.com>
2015-08-26 12:41:55 -0700

    Implement O(n^2) + O(nlog(n)) algorithm for finding pythagorean triplet.

--- lib/myarray.rb
+++ lib/myarray.rb
@@ -19,16 +19,30 @@ class MyArray
     # a^2 + b ^2 = c ^2
     result = []
     len = @data.length
-    i = 0
-    while i < len - 1 do
-      unless i + 2 > len - 1
-        a = @data[i]
-        b = @data[i+1]
-        c = @data[i+2]
-        sum = a**2 + b**2
-        result << [a,b,c] if sum == c**2
+    k = len - 1
+    data = @data.sort
+
+    while k >= 2  do
+      i = 0
+      j = k - 1
+      z = data[k]
+      puts "k: #{k}"
+      while (i < k - 1 && i < j) do
+        x = data[i]
+        y = data[j]
+        sum = x**2 + y**2
+        if sum < z**2
+          i += 1
+        elsif  sum > z**2
+          j -= 1
+        else
+          result << [x,y,z]
+          i += 1
+          j -= 1
+        end
+        puts "i: #{i} and j: #{j}"
       end
-      i += 1
+      k -= 1
     end
     result
   end
--- spec/myarray_spec.rb
+++ spec/myarray_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper'
 
 describe MyArray do
-
   describe '#find_2nd_max' do
     before do
       @my_arr = MyArray.new [3, 14, 5, 6, 2, 1, 11, 20, 16]
@@ -12,17 +11,14 @@ describe MyArray do
   end
   
   describe '#pie_theory' do
-    before do
-      @my_arr = MyArray.new [9, 2, 3, 4, 8, 5, 6, 10]
-    end
-
     it 'should find array of three items that matches pie_theory' do
-      expect(@my_arr.pie_theory).to eq [[3,4,5]]
+      @my_arr = MyArray.new [9, 2, 3, 4, 8, 5, 6, 10]
+      expect(@my_arr.pie_theory).to eq [[6,8,10],[3,4,5]]
     end
 
-    # it 'should return empty array if no match' do
-    #   
-    # end
+    it 'should return empty array if no match' do
+      @my_arr = MyArray.new [9, 1, 3, 8, 5, 10]
+      expect(@my_arr.pie_theory).to eq []
+    end
   end
-
 end
